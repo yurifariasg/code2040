9692b76e4af7f0a06094181a21c39dddbac8e033
unknown <kayalew@mit.edu>
2012-08-30 20:14:19 -0400

    added fields and cosntraints to recruitment info domain

--- CODE2040Dashboard/grails-app/domain/org/code2040/dashboard/RecruitmentInfo.groovy
+++ CODE2040Dashboard/grails-app/domain/org/code2040/dashboard/RecruitmentInfo.groovy
@@ -2,6 +2,13 @@ package org.code2040.dashboard
 
 class RecruitmentInfo {
 
-    static constraints = {
-    }
+	int candidateID//person who suggested the potential recruit
+	String fname
+	String lname
+ 	String email
+	String title
+	static constraints = {
+		fname minSize:2, blank:false, nullable:true
+		lname minSize:2, blank:false, nullable:true
+	}
 }
