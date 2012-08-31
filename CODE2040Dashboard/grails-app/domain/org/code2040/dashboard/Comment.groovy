14974666189c08dfdcc915d563c093f0280c7766
unknown <kayalew@mit.edu>
2012-08-30 20:15:45 -0400

    added fields to comment domain

--- CODE2040Dashboard/grails-app/domain/org/code2040/dashboard/Comment.groovy
+++ CODE2040Dashboard/grails-app/domain/org/code2040/dashboard/Comment.groovy
@@ -4,6 +4,11 @@ class Comment {
 
 	ApplicationStep step
 	String comment
+	int mangagerId
+	int approverManagerId
+	int timeCreated = System.currentTimeMillis()
+	boolean resolved = false
+	
 	
     static constraints = {
 		step minSize:3, blank:false, nullable:true
